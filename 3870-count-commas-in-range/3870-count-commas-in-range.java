8d122885232c18ebfffd0bf2b401c1c79d805211
Granth rana <granthrana6@gmail.com>
2026-09-09 14:44:30 +0530

    Time: 1 ms (83.58%), Space: 42.6 MB (56.6%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,12 +1,6 @@
 class Solution {
     public int countCommas(int n) {
-        int max =0;
-        if(n<1000){
-            max=0;
-        }else if (n>=1000){
-            max=n-1000+1;
-        }
-        return max;
+         return Math.max(n-999,0);
     }
     
 }
